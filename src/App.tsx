1805360c5a5528a6ddfcd7a79b7eacda69c1e872
Shehwaz81 <shehwazlongia@gmail.com>
2025-08-04 01:00:29 -0400

    fixed file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
-import { useState } from 'react'
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite' 
-import './App.css'
+import Navbar from './components/Navbar'
+import './index.css'
 
-const Navbar = () => {
+export const App = () => {
   return (
-    <nav className="fixed top-3 left-1/2 transform -translate-x-1/2 z-50">
-      <div className="bg-black/80 w-96 backdrop-blur-md border-3 border-green-700 rounded-3xl h-12 flex justify-center items-center text-white transition duration-300 hover:scale-102">
-        <div className="flex space-x-8">
-          <a href="#home" className="text-white/70 hover:text-white transition-colors text-sm">Home</a>
-          <a href="#about" className="text-white/70 hover:text-white transition-colors text-sm">About</a>
-          <a href="#projects" className="text-white/70 hover:text-white transition-colors text-sm">Projects</a>
-          <a href="#contact" className="text-white/70 hover:text-white transition-colors text-sm">Contact</a>
-        </div>
-      </div>
-    </nav>
+    <Navbar/>
   )
 }
 
-export default Navbar
+export default App 
